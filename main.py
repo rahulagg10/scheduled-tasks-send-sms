bac5d742041851f03e67ddb7e1423beb04cdb9f3
rahulagg10 <rahulagg10@gmail.com>
2026-03-13 23:00:59 +0530

    Uncomment message creation and print response

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ account_sid = "ACdbf284cd6bb78d55287ddd29654f9b1e"
 auth_token = "f3780a425707dc84bff691b51a009909"
 client = Client(account_sid, auth_token)
 
-# message = client.messages.create(
-#     body="Aaj Night ka kya plan h?",
-#     from_="+16562263135",
-#     to="+918800149444",
-# )
-print("SID:", account_sid[:6])
-print("Token length:", len(auth_token))
-print(client.api.accounts(account_sid).fetch().friendly_name)
-# print(message.body)
+message = client.messages.create(
+    body="Aaj Night ka kya plan h?",
+    from_="+16562263135",
+    to="+918800149444",
+)
+# print("SID:", account_sid[:6])
+# print("Token length:", len(auth_token))
+# print(client.api.accounts(account_sid).fetch().friendly_name)
+print(message.body)
